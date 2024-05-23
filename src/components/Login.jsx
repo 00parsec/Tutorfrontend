479cad8f20c87dc018f6aa2f267dc172dbfc6eef
00parsec <137364055+00parsec@users.noreply.github.com>
2024-05-23 17:48:18 -0400

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,7 +12,8 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:3000/api/login', {
+      //const response = await fetch('http://localhost:3000/api/login', {
+      const response = await fetch('https://tutorbackend-fnxu.onrender.com/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
