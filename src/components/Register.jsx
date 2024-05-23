d317d6863df501c37c46c19878c5d65cd2cf31ca
00parsec <137364055+00parsec@users.noreply.github.com>
2024-05-23 17:57:36 -0400

    Update Register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -31,13 +31,14 @@ function Register() {
 
     try {
      // const response = await fetch('http://localhost:3000/api/register', {
-      const response = await fetch('https://tutorbackend-fnxu.onrender.com/api/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userData),
-      });
+    const response = await fetch('https://tutorbackend-fnxu.onrender.com/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    });
+
 
       if (response.ok) {
         const jsonResponse = await response.json();
